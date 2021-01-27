8222bba1d23011ad9ac76d7506e99ab6ba815bac
Unubayar <unuu0430@gmail.com>
2021-01-28 00:44:11 +0800

    Finance model ruu item add hissen

---
+++ app.js
@@ -0,0 +1,103 @@
+//  desktop work controller
+var uiController = (function () {
+
+    var DOMstrings = {
+        inputType : ".add__type",
+        inputDescription: ".add__description",
+        inputValue: ".add__value",
+        addBtn: ".add__btn"
+    }
+    
+    return {
+        getInput: function (){
+            return {
+                type : document.querySelector(DOMstrings.inputType).value, //exp, inc
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+        getDOMStrings: function(){
+         return DOMstrings;
+        }
+    }
+}) ();
+// Sanhuu work controller
+var financeController = (function () {
+    var Income = function (id , descreption, value){
+        this.id = id;
+        this.descreption = descreption;
+        this.value = value;
+      
+      }
+      var Expense = function (id , descreption, value){
+        this.id = id;
+        this.descreption = descreption;
+        this.value = value;
+      
+      }
+      var data = {
+        item : {
+          inc: [],
+          exp: [],
+        },
+        totals: {
+          inc: 0,
+          exp: 0
+        }
+      };
+      return {
+          addItem: function(type, desc, val){
+              var item, id ;
+              
+              if (data.item[type].length === 0) id = 1; 
+              else {
+                 id = data.item[type][data.item[type].length - 1].id + 1;
+              }
+              if (type === "inc"){
+                  item = new Income(id, desc, val);
+              }else {
+                  item = new Expense(id, desc, val)
+              }
+
+              data.item[type].push(item);
+          }
+      }
+
+}) ();
+// programm holbogch controller
+var appController = (function (uiController, financeController) {
+
+    var ctrlAddItem = function(){
+        // 1. Оруулах өгөгдлийн дэлгэцээс олж авна.
+         var input = uiController.getInput();
+        // 2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална.
+        financeController.addItem(input.type, input.description, input.value);
+
+        // 3. Олж авсан өгөгдлүүдээ вэб дээрээ тохирох хэсэгт нь гаргана..
+        // 4. Төсвийг тооцоолно.
+        // 5. Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана.
+    }
+
+     var setupEvenListeners = function (){
+        var DOM = uiController.getDOMStrings();
+
+        document.querySelector(DOM.addBtn).addEventListener('click', function(){
+            ctrlAddItem();
+        });
+   
+       document.addEventListener("keypress", function(event){
+       if (event.keyCode === 13 || event.which === 13) {
+           ctrlAddItem();
+       }
+       });
+     };
+     return {
+         init: function (){
+             console.log("Application started...");
+             setupEvenListeners();
+         }
+     }
+
+}) (uiController, financeController);
+ 
+appController.init();
