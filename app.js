c473c53eb67267b0ed37b02315c3ac12445c3cf9
Unubayar <unuu0430@gmail.com>
2021-01-30 22:18:23 +0800

    inc exp array

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ var uiController = (function () {
         inputType : ".add__type",
         inputDescription: ".add__description",
         inputValue: ".add__value",
-        addBtn: ".add__btn"
+        addBtn: ".add__btn",
+        incomeList : ".income__list",
+        expensesList: ".expenses__list"
     }
     
     return {
@@ -13,11 +15,46 @@ var uiController = (function () {
             return {
                 type : document.querySelector(DOMstrings.inputType).value, //exp, inc
                 description: document.querySelector(DOMstrings.inputDescription).value,
-                value: document.querySelector(DOMstrings.inputValue).value
+                value: parseInt(document.querySelector(DOMstrings.inputValue).value),
             };
         },
         getDOMStrings: function(){
          return DOMstrings;
+        },
+        clearFields: function(){
+           var fields = document.querySelectorAll(
+               DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+
+            //    Convert List to Array
+            var fieldsArr = Array.prototype.slice.call(fields);
+
+            fieldsArr.forEach(function (el, index, array){
+                el.value= '';
+            });
+
+            // for(var i = 0; i < fieldsArr.length; i++){
+            //     fieldsArr[i].value = "";
+            // }
+
+            fieldsArr[0].focus();
+        },
+        addListItem: function (item, type){
+            // Income Expense elementiig aguulsan html prepare
+            var html, list;
+            if (type === "inc"){
+                list = DOMstrings.incomeList;
+                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">$$DESCREPTION$$</div><div class="right clearfix"><div class="item__value">$$value$$</div> <div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div> </div></div>';
+               
+            }else{
+                list = DOMstrings.expensesList;
+                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">$$DESCREPTION$$</div><div class="right clearfix"><div class="item__value">$$value$$</div><div class="item__percentage">21%</div><div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div></div> </div>';
+            }
+            // Replace use change
+            html = html.replace('%id%', item.id);
+            html = html.replace('$$DESCREPTION$$', item.descreption);
+            html = html.replace('$$value$$', item.value)
+            // DOM ru oruulah
+            document.querySelector(list).insertAdjacentHTML('beforeend', html);
         }
     }
 }) ();
@@ -60,7 +97,13 @@ var financeController = (function () {
               }
 
               data.item[type].push(item);
-          }
+
+              return item;
+
+          },
+          seeData: function (){
+              return data;
+          },
       }
 
 }) ();
@@ -70,12 +113,18 @@ var appController = (function (uiController, financeController) {
     var ctrlAddItem = function(){
         // 1. Оруулах өгөгдлийн дэлгэцээс олж авна.
          var input = uiController.getInput();
-        // 2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална.
-        financeController.addItem(input.type, input.description, input.value);
+{
+         if(input.description !== "" && input.value !== ""){
+             // 2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална.
+        var item = financeController.addItem(input.type, input.description, input.value);
 
         // 3. Олж авсан өгөгдлүүдээ вэб дээрээ тохирох хэсэгт нь гаргана..
+        uiController.addListItem(item, input.type)
+        uiController.clearFields();
         // 4. Төсвийг тооцоолно.
         // 5. Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана.
+         }
+        
     }
 
      var setupEvenListeners = function (){
